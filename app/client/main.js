92c4b4be2b2d30871fb0ae991e4fbafc4ce574f0
mserai <mserai@hawaii.edu>
2017-10-25 13:13:57 -1000

    hope i dont mess up

--- app/client/main.js
+++ app/client/main.js
@@ -5,3 +5,4 @@ import '/imports/ui/layouts';
 import '/imports/ui/pages';
 import '/imports/ui/components/form-controls/';
 import '/imports/ui/stylesheets/style.css';
+//This is Philip's Stuff not Mike's

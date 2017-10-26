7d407c0b37fb9cb420d5db93f78db16fa424e9b2
mserai <mserai@hawaii.edu>
2017-10-26 10:52:28 -1000

    fin wod

--- app/imports/ui/pages/edit-contact-page.js
+++ app/imports/ui/pages/edit-contact-page.js
@@ -52,5 +52,10 @@ Template.Edit_Contact_Page.events({
       instance.messageFlags.set(displayErrorMessages, true);
     }
   },
+  'click.delete'(event, instance) {
+    event.preventDefault();
+    Contacts.remove(FlowRouter.getParam('_id'), { instance });
+    FlowRouter.go('Home_Page');
+  },
 });
 
